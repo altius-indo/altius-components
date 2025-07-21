f7eedc22cd3d5238c49f1aef7110ae0d5ca65f69
jsanyal <subhajyoti@gmail.com>
2025-07-20 17:48:36 -0700

    Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
+<script>
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
 <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Create your package using @sveltejs/package and preview/showcase your work
+  with SvelteKit
+</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+<Button
+  href="https://svelte.dev/docs/kit"
+  variant="default"
+  size="lg"
+  class="bg-brand-background  hover:bg-brand-800"
+>
+  Click Me
+</Button>
+<Button href="https://svelte.dev/docs/kit" variant="default" size="lg">
+  another one
+</Button>
